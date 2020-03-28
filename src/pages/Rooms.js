5404b632b97ba5a138546ff01876aa8bc9f07f40
globalexploit <62809081+globalexploit@users.noreply.github.com>
2020-03-28 23:58:41 +0000

    react seahorse application

---
+++ src/pages/Rooms.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import RoomContainer from "../components/RoomContainer";
+
+export default class Rooms extends Component {
+  render() {
+    return (
+      <>
+        <Hero hero="roomsHero">
+          <Banner title="our rooms">
+            <Link to="/" className="btn-primary">
+              Return Home
+            </Link>
+          </Banner>
+        </Hero>
+        <RoomContainer />
+      </>
+    );
+  }
+}
